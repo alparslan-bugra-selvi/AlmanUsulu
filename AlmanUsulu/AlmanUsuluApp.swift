26c438bec2a9ba6429f1374e48c4966ec87a5277
Alparslan Buğra Selvi <alparslan.bugra@hotmail.com>
2023-08-03 21:15:21 +0300

    Initial Commit

---
+++ AlmanUsulu/AlmanUsuluApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmanUsuluApp.swift
+//  AlmanUsulu
+//
+//  Created by Alp Selvi on 3.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlmanUsuluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
